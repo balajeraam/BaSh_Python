309775b3702006e6c37218c06c85e33f6a302d5f
Balaje Shrinivaas <61991584+balajeraam@users.noreply.github.com>
2021-09-05 17:58:42 +0200

    Create Mars_weather_Tkinter_GUI.py

---
+++ Space/Mars_weather_Tkinter_GUI.py
@@ -0,0 +1,52 @@
+from tkinter import *
+import tkinter as tk
+import requests
+import json
+
+response = requests.get("https://api.maas2.apollorion.com").json()
+sol = response['sol']
+day = int(sol * (3299/3212))
+
+win = Tk()
+win.title("Mars Weather Forecast")
+win.geometry("400x350")
+win.config(bg='grey')
+
+
+def LabelTemplate(texte):
+    label_name = Label(win,text=texte,font=("calibri",12,"bold"),bg="grey",foreground="white")
+    label_name.pack(anchor = W)
+
+
+LabelTemplate(texte="Time passed since the landing of Curiosity Rover")
+
+star="*"*42
+LabelTemplate(texte=star)
+
+mars_days=">> "+str(sol)+" Martian days (Sols) passed"
+LabelTemplate(texte=mars_days)
+
+earth_days=">> "+str(day)+" Earth days passed"
+LabelTemplate(texte=earth_days)
+
+blank=" "
+LabelTemplate(texte=blank)
+
+weather_forecast = "Weather forecast for Tosol (equivalent of today) "
+LabelTemplate(texte=weather_forecast)
+
+LabelTemplate(texte=star)
+
+min_temp = ">> Minimum temperature : "+ str(response['min_temp']) + " degree Celsius"
+LabelTemplate(texte=min_temp)
+
+max_temp = ">> Maximum temperature : "+ str(response['max_temp']) + " degree Celsius"
+LabelTemplate(texte=max_temp)
+
+sunrise = ">> Sunrise at          : " + str(response['sunrise'])
+LabelTemplate(texte=sunrise)
+
+sunset = ">> Sunset at           : " + str(response['sunset'])
+LabelTemplate(texte=sunset)
+
+win.mainloop()
