e9e6c02e82b3cd89a9cdbdb2977819ba29c9a8cd
Balaje Shrinivaas <61991584+balajeraam@users.noreply.github.com>
2021-09-17 16:36:45 +0200

    Update Level1_Ping_plus_LogFile.py

--- Ping_Multiple_IP/Level1_Ping_plus_LogFile.py
+++ Ping_Multiple_IP/Level1_Ping_plus_LogFile.py
@@ -3,7 +3,7 @@ import openpyxl
 import time
 import datetime
 import subprocess
-
+import platform
 
 # Requesting user to provide the path where XLSX file containing server ip list is stored
 path = input("Enter path where .XLSX containing server list is stored  >> ")
@@ -11,22 +11,32 @@ path = input("Enter path where .XLSX containing server list is stored  >> ")
 # In Windows separator used is "\". We need to modify it to separator "/" that python understands
 path = path.replace(os.sep,"/")
 
+# Loading active worksheet in the .xlsx file
 wb_obj = openpyxl.load_workbook(path)
 sheet_obj = wb_obj.active
+
+# In our excel file first row is header. So range starts from 2.
 for row in range (2,sheet_obj.max_row+1):
     
     ip_address_cell = sheet_obj.cell(row,1)
-    # parameter -n is for windows machine
-    command = ['ping','-n','3', ip_address_cell.value]
+    
+    # Building the command that will be used to ping the ipaddress
+    if platform.system().lower()=="windows" :
+        parameter ='-n'
+    else :
+        parameter ='-c'
+    command = ['ping',parameter,'3', ip_address_cell.value]
    
     run_time = datetime.datetime.now()
     timestamp_cell = sheet_obj.cell(row,4)
     timestamp_cell.value = run_time
-
+    
+    # Ping and obtain system code
     outcome = subprocess.call((command),stdout=subprocess.DEVNULL)
     ip_status_cell = sheet_obj.cell(row,5)
     ip_status_cell.value = outcome
-
+    
+    # Translating the status code into comments that can also help a non-technical personal
     comment_cell = sheet_obj.cell(row,6)
     if outcome == 0 :
         comment_cell.value = "Reachable"
@@ -34,4 +44,5 @@ for row in range (2,sheet_obj.max_row+1):
         comment_cell.value = "Not Reachable"
     
     wb_obj.save(path)
-    time.sleep(4)
+    # Small wait time provided after update for ipaddress is complete
+    time.sleep(2)
