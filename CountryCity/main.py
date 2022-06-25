379a50644a990dc42b6dff0cab2ff6d2b0adaf98
balajeraam <balajepersonal@gmail.com>
2022-06-25 15:44:26 +0200

    finetuning

--- CountryCity/main.py
+++ CountryCity/main.py
@@ -21,17 +21,17 @@ async def read(request: Request):
 
 @app.post("/rr",response_class= HTMLResponse)
 async def handle_form(request: Request, country: str = Form(...), city: str = Form(...)):
-    if country == "USA".lower():
+    if country == "USA" and city == "ARKANSAS" :
         image_file1 = "Arkansas1.JPG"
         image_file2 = "Arkansas2.JPG"
-    elif country == "India".lower():
+    elif country == "INDIA" and city == "TRICHY":
         image_file1 = "Trichy1.JPG"
         image_file2 = "Trichy2.JPG"
     else:
         image_file1 ="BaselSBB.JPG"
         image_file2 ="MB.PNG"
     
-    return templates.TemplateResponse("RightPrint_output.html",{"request": request , "id_country":country, "id_city":city, "image_file1": image_file1, "image_file2": image_file2})
+    return templates.TemplateResponse("RightPrint_output.html",{"request": request , "id_country":country, "id_city":city, "image_file1": image_file1, "image_file2": image_file2}) 
     
 
 
