b93ab741df14966a34e82fee3a775376f933fa58
balajeraam <balajepersonal@gmail.com>
2022-06-25 16:29:40 +0200

    python html css file

---
+++ CountryCityVirtual/main.py
@@ -0,0 +1,40 @@
+from fastapi import FastAPI, Request , File, UploadFile , Form
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
+from requests import request
+
+app = FastAPI()
+
+templates = Jinja2Templates(directory="htmldirectory")
+app.mount("/static",StaticFiles(directory="static"), name="static")
+
+@app.get("/")
+async def hello_world():
+    return {"message": "Hello World"}
+
+
+@app.get("/rr",response_class=HTMLResponse)
+async def read(request: Request):
+    
+    return templates.TemplateResponse("RightPrint_home.html",{"request": request})
+
+@app.post("/rr",response_class= HTMLResponse)
+async def handle_form(request: Request, country: str = Form(...), city: str = Form(...)):
+    if country == "USA" and city == "ARKANSAS" :
+        image_file1 = "Arkansas1.JPG"
+        image_file2 = "Arkansas2.JPG"
+    elif country == "INDIA" and city == "TRICHY":
+        image_file1 = "Trichy1.JPG"
+        image_file2 = "Trichy2.JPG"
+    else:
+        image_file1 ="BaselSBB.JPG"
+        image_file2 ="MB.PNG"
+    
+    return templates.TemplateResponse("RightPrint_output.html",{"request": request , "id_country":country, "id_city":city, "image_file1": image_file1, "image_file2": image_file2}) 
+    
+
+
+
+
+
